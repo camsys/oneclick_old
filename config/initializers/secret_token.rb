9dcdec2affb597c004f7374761bf8371b015a1a5
denishaskin <denis@constantorbit.com>
2013-05-01 14:43:40 -0400

    rails new...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneclick::Application.config.secret_token = '33d7c920b675cedd2e60a3597051fb11b7c9f6aa3ce6bb26aa76c1513a5a3d337661433c370cc5067f342b50d497c78b01d5e62603bf52d4d5b91a577e724bcf'
