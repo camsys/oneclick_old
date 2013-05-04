37a9030098d5f4e73fc52d8c9f3d991e897b801a
denishaskin <denis@constantorbit.com>
2013-05-03 21:12:35 -0400

    Home buttons all work correctly, responsive, etc.  Alt tags and null links for now.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   layout 'no_sidebar'
 
   def home
+    @actions = [
+        {img: 'plan-a-trip.png', label: 'Plan a Trip', target: '#'},
+        {img: 'identify-places.png', label: 'Identify Places', target: '#'},
+        {img: '100px-Settings_Tile.svg.png', label: 'Change My Settings', target: '#'},
+        {img: 'Blue_question_mark_icon.png', label: 'Help & Support', target: '#'},
+    ]
   end
 
 end
